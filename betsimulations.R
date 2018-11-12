77dff876b8befd95eda06bd941fac7876ceec01e
JEFF PHILLIPS <jeff.phillips@agilent.com>
2018-11-11 20:28:29 -0500

    Readability Updates

--- betsimulations.R
+++ betsimulations.R
@@ -1,9 +1,35 @@
 library(tidyverse)
+library(RColorBrewer)
 
+# Define colors for lines in graphs to follow
+my_colors <- brewer.pal(6, "Dark2")
+
+# Expected Logarithmic Growth
 EV <- function(odds, probability, bankroll, bet){
   probability * log(1 + (odds * bet / bankroll)) + (1 - probability) * log(1 - (bet / bankroll))
 }
 
+# Find the two values in haystack that 'bookend' needle
+# Returns a vector of length 2
+find_bookends <- function(needle, haystack){
+  haystack = sort(haystack)
+  # If all of the values in haystack are bigger than needle, return the max value twice
+  if(!any(haystack > needle)){
+    return (c(max(haystack), max(haystack)))
+  }
+  # If all of the values in haystack are smaller than needle, return the min value twice
+  if(!any(haystack < needle)){
+    return (c(min(haystack), min(haystack)))
+  }
+  # Otherwise, iterate through the list to find the pivot
+  for (i in 1:(length(haystack) - 1)){
+    if(haystack[i] <= needle & haystack[i+1] >= needle){
+      return (c(haystack[i], haystack[i+1]))
+    }
+  }
+}
+
+# Betting strategy: pick a random bet out of the set of discrete options
 random_bet <- function(bankroll, bet){
   bet_possible = bet[bet <= bankroll]
   if(length(bet_possible) > 0){
@@ -14,89 +40,94 @@ random_bet <- function(bankroll, bet){
   }
 }
 
+# Betting strategy: pick the highest bet from the discrete options that can be afforded
 max_bet <- function(bankroll, bet){
   bet_possible = bet[bet <= bankroll]
-  if(length(bet_possible) > 0){
-    max(bet_possible)
-  }
-  else{
-    0
-  }
+  ifelse(bankroll >= finalbet, finalbet, 0)
+  
 }
 
+# Betting strategy: bet the kelly bet, regardless of the discrete options
 kelly_bet <- function(odds, probability, bankroll) {
   finalbet = ((probability * (odds + 1) - 1) / odds) * bankroll
-  if(bankroll >= finalbet){
-    finalbet
-  }
-  else{
-    0
-  }
+  ifelse(bankroll >= finalbet, finalbet, 0)
 }
 
+# Betting strategy: use suggested methodology to make kelly bet
 best_fixed_bet <- function(odds, probability, bankroll, bet){
   result = data.frame(bet) %>%
     mutate(expected_value = EV(odds, probability, bankroll, bet))
   result <- result[order(-result$expected_value),]
   finalbet = result$bet[[1]]
-  if(bankroll >= finalbet){
-    finalbet
-  }
-  else{
-    0
-  }
+  ifelse(bankroll >= finalbet, finalbet, 0)
+  
 }
 
+# Betting strategy: select randomly from the bets that bookend the kelly bet
 best_two_bets <- function(odds, probability, bankroll, bet){
-  kelly_bet = kelly_bet(odds, probability, bankroll)
-  result = data.frame(bet) %>%
-    mutate(kelly_difference = abs(bet - kelly_bet))
-  result <- result[order(result$kelly_difference),]
-  best_bets <- c(result$bet[[1]], result$bet[[2]])
+  kellybet = kelly_bet(odds, probability, bankroll)
+  best_bets <- find_bookends(kellybet, bet)
   decision_RV <- sample(1:2, 1)
   finalbet = best_bets[decision_RV]
-  if(bankroll >= finalbet){
-    finalbet
-  }
-  else{
-    0
-  }
+  ifelse(bankroll >= finalbet, finalbet, 0)
+  
 }
 
+# Betting strategy: select the bookend bet closest to the kelly bet
+closest_bookend <- function(odds, probability, bankroll, bet){
+  kellybet <- kelly_bet(odds, probability, bankroll)
+  best_bets <- find_bookends(kellybet, bet)
+  finalbet <- best_bets[which(abs(best_bets-kellybet)==min(abs(best_bets-kellybet)))]
+  ifelse(bankroll >= finalbet, finalbet, 0)
+  
+}
+
+# Simulate a run and output the bankrolls associated with various betting strategies
 bet_sim <- function(initial_bankroll, odds, probability, possible_bets, num_bets){
+  # Determine wins and losses
   bet_result = c("N/A", rbinom(num_bets, 1, probability))
   
+  # Initialize bankroll vectors
   kelly_bankroll = integer(length(num_bets))
   discrete_kelly_bankroll = integer(length(num_bets))
   modified_discrete_kelly_bankroll = integer(length(num_bets))
   random_discrete_bankroll = integer(length(num_bets))
   max_discrete_bankroll = integer(length(num_bets))
+  closest_bookend_bankroll = integer(length(num_bets))
   
+  # Set the initial bankrolls
   kelly_bankroll[1] = initial_bankroll
   discrete_kelly_bankroll[1] = initial_bankroll
   modified_discrete_kelly_bankroll[1] = initial_bankroll
   random_discrete_bankroll[1] = initial_bankroll
   max_discrete_bankroll[1] = initial_bankroll
+  closest_bookend_bankroll[1] = initial_bankroll
   
+  # Bet num_bets times
   for (i in 2:(num_bets + 1)){
+    # i.e. the bettor wins
     if(bet_result[i - 1] == 1){
       kelly_bankroll[i] = (kelly_bankroll[i-1] + odds * kelly_bet(odds, probability, kelly_bankroll[i-1]))
       discrete_kelly_bankroll[i] = (discrete_kelly_bankroll[i-1] + odds * best_fixed_bet(odds, probability, discrete_kelly_bankroll[i-1], possible_bets))
       modified_discrete_kelly_bankroll[i] = (modified_discrete_kelly_bankroll[i-1] + odds * best_two_bets(odds, probability, modified_discrete_kelly_bankroll[i-1], possible_bets))
       random_discrete_bankroll[i] = (random_discrete_bankroll[i-1] + odds * random_bet(random_discrete_bankroll[i-1], possible_bets))
       max_discrete_bankroll[i] = (max_discrete_bankroll[i-1] + odds * max_bet(max_discrete_bankroll[i-1], possible_bets))
+      closest_bookend_bankroll[i] = (closest_bookend_bankroll[i-1] + odds * closest_bookend(odds, probability, closest_bookend_bankroll[i-1], possible_bets))
     }
+    # i.e. the bettor loses
     else{
       kelly_bankroll[i] = (kelly_bankroll[i-1] - kelly_bet(odds, probability, kelly_bankroll[i-1]))
       discrete_kelly_bankroll[i] = (discrete_kelly_bankroll[i-1] - best_fixed_bet(odds, probability, discrete_kelly_bankroll[i-1], possible_bets))
       modified_discrete_kelly_bankroll[i] = (modified_discrete_kelly_bankroll[i-1] - best_two_bets(odds, probability, modified_discrete_kelly_bankroll[i-1], possible_bets))
       random_discrete_bankroll[i] = (random_discrete_bankroll[i-1] - random_bet(random_discrete_bankroll[i-1], possible_bets))
       max_discrete_bankroll[i] = (max_discrete_bankroll[i-1] - max_bet(max_discrete_bankroll[i-1], possible_bets))
-      }
+      closest_bookend_bankroll[i] = (closest_bookend_bankroll[i-1] - closest_bookend(odds, probability, closest_bookend_bankroll[i-1], possible_bets))
+    }
   }
-  data.frame(time = 0:(num_bets), bet_result, kelly_bankroll, discrete_kelly_bankroll, modified_discrete_kelly_bankroll, random_discrete_bankroll, max_discrete_bankroll)
+  data.frame(time = 0:num_bets, bet_result, kelly_bankroll, discrete_kelly_bankroll, modified_discrete_kelly_bankroll, random_discrete_bankroll, max_discrete_bankroll, closest_bookend_bankroll)
 }
 
+# Simulate a series of runs and take the average bankroll at each step
 multiple_sim <- function(initial_bankroll, odds, probability, possible_bets, num_bets, num_sims){
   result = data.frame()
   for(sim in 1:num_sims){
@@ -109,18 +140,20 @@ multiple_sim <- function(initial_bankroll, odds, probability, possible_bets, num
               discrete_kelly_bankroll = mean(discrete_kelly_bankroll),
               modified_discrete_kelly_bankroll = mean(modified_discrete_kelly_bankroll),
               random_discrete_bankroll = mean(random_discrete_bankroll),
-              max_discrete_bankroll = mean(max_discrete_bankroll))
+              max_discrete_bankroll = mean(max_discrete_bankroll),
+              closest_bookend_bankroll = mean(closest_bookend_bankroll))
 }
 
-set.seed(11)
+set.seed(0)
 data_run = bet_sim(initial_bankroll = 10, odds = .8, probability = .6, possible_bets = c(1, 2, 5, 10, 25, 50), num_bets = 100)
 
-ggplot(data_run, aes(time)) + 
+ggplot(data_run, aes(x = time)) + 
   geom_line(aes(y = kelly_bankroll, colour = "Theoretical Kelly"), linetype = "dashed") + 
   geom_line(aes(y = discrete_kelly_bankroll, colour = "Discrete Kelly")) + 
-  geom_line(aes(y = modified_discrete_kelly_bankroll, colour = "Modified Discrete Kelly")) + 
+  geom_line(aes(y = modified_discrete_kelly_bankroll, colour = "Random Bookend Bet: Modified Discrete Kelly")) + 
   geom_line(aes(y = random_discrete_bankroll, colour = "Random Discrete Bet")) + 
   geom_line(aes(y = max_discrete_bankroll, colour = "Max Discrete Bet")) + 
+  geom_line(aes(y = closest_bookend_bankroll, colour = "Closest Bookend Bet: Modified Discrete Kelly")) + 
   ggtitle("Simulation Study (Single Run)", subtitle = "(Probability = .6, Odds = .8, Bets = {1, 2, 5, 10, 25, 50})") + 
   scale_colour_manual(name  ="Bet Strategy", values = my_colors) +
   ylab("Bankroll") + 
@@ -128,7 +161,7 @@ ggplot(data_run, aes(time)) +
   theme_bw() + 
   theme(legend.position = c(.2, .6))
 
-set.seed(0)
+set.seed(1000)
 multiple_runs = multiple_sim(initial_bankroll = 100, 
                         odds = .8, 
                         probability = .6, 
@@ -139,12 +172,14 @@ multiple_runs = multiple_sim(initial_bankroll = 100,
 ggplot(multiple_runs, aes(time)) + 
   geom_line(aes(y = kelly_bankroll, colour = "Theoretical Kelly"), linetype = "dashed") + 
   geom_line(aes(y = discrete_kelly_bankroll, colour = "Discrete Kelly")) + 
-  geom_line(aes(y = modified_discrete_kelly_bankroll, colour = "Modified Discrete Kelly")) + 
+  geom_line(aes(y = modified_discrete_kelly_bankroll, colour = "Random Bookend Bet: Modified Discrete Kelly")) + 
   geom_line(aes(y = random_discrete_bankroll, colour = "Random Discrete Bet")) + 
   geom_line(aes(y = max_discrete_bankroll, colour = "Max Discrete Bet")) + 
+  geom_line(aes(y = closest_bookend_bankroll, colour = "Closest Bookend Bet: Modified Discrete Kelly")) +  
   scale_colour_manual(name  ="Bet Strategy", values = my_colors) +
   ggtitle("Simulation Study (Averaged Over 1000 Runs)", subtitle = "(Probability = .6, Odds = .8, Bets = {1, 2, 5, 10, 25, 50})") + 
   ylab("Bankroll") + 
   xlab("Time") + 
+  ylim(0, 200) +
   theme_bw() + 
   theme(legend.position = c(.8, .3))
