b02006dcadd93f4d8c338ae290e0fdb83a74fbc6
Jeff Phillips <jgp2053@gmail.com>
2020-08-28 23:35:49 -0400

    automatically save graphs

--- kellyplots.R
+++ kellyplots.R
@@ -1,5 +1,4 @@
 library(tidyverse)
-library(RColorBrewer)
 
 log_utility <- function(bankroll, amount, probability, odds) {
   return_this <- ifelse(amount >= bankroll, NA, 
@@ -63,7 +62,7 @@ maxes = univ %>%
          best_bet = amount_color, 
          best_utility = utility)
 
-univ %>%
+graph_varyp <- univ %>%
   left_join(maxes) %>%
   ggplot(aes(x = probability, y = utility)) +
   geom_line(aes(color = amount_color, linetype = is_kelly), size = 1) +
@@ -79,8 +78,8 @@ univ %>%
   guides(linetype = FALSE, fill = FALSE, size = FALSE) +
   xlab("Probability") + 
   ylab("Logarithmic Utility") + 
-  ggtitle("Expected Logarithmic Utility with Varying Probability", 
-          subtitle = "(Odds = .8, Bankroll = 100)") + 
+  # ggtitle("Expected Logarithmic Utility with Varying Probability", 
+  #         subtitle = "(Odds = .8, Bankroll = 100)") + 
   theme_bw() +
   geom_point(aes(x=0.7245419, y=0.05285527)) +
   geom_text(aes(x=0.7245419 + x_offset, y=0.05285527 + y_offset, label = 'B')) +
@@ -88,6 +87,14 @@ univ %>%
   geom_text(aes(x=0.6337606 + x_offset, y=0.0101877 + y_offset, label = 'A')) +
   geom_point(aes(x=2/3, y=0.02565368)) +
   geom_text(aes(x=2/3 + x_offset, y=0.02565368 + y_offset, label = 'C'))
+
+ggsave(
+  file = 'varyp.png', 
+  plot = graph_varyp, 
+  width= 6, 
+  height = 4, 
+  path = 'intersection_plots'
+)
 
 # Varying Odds
 
@@ -114,7 +121,7 @@ maxes = univ %>%
   slice(which.max(utility)) %>%
   select(odds, probability, bankroll, best_bet = amount_color, best_utility = utility)
 
-univ %>%
+graph_varya <- univ %>%
   left_join(maxes) %>%
   ggplot(aes(x = odds, y = utility)) +
   geom_line(aes(color = amount_color, linetype = is_kelly), size = 1) + 
@@ -127,7 +134,7 @@ univ %>%
   scale_linetype_manual(values = c("Kelly" = "dashed", "non-Kelly" = "solid")) + 
   guides(linetype = FALSE, fill = FALSE) +
   xlab("Odds") + ylab("Logarithmic Utility") + 
-  ggtitle("Expected Logarithmic Utility with Varying Odds", subtitle = "(Probability = .4, Bankroll = 100000)") + 
+  # ggtitle("Expected Logarithmic Utility with Varying Odds", subtitle = "(Probability = .4, Bankroll = 100000)") + 
   theme_bw() +
   geom_point(aes(x=1.62132, y=0.0004030681)) +
   geom_text(aes(x=1.62132 + x_offset, y=0.0004030681 + y_offset, label = 'B')) +
@@ -136,24 +143,41 @@ univ %>%
   geom_point(aes(x=1.8, y=0.003695102)) +
   geom_text(aes(x=1.8 + x_offset, y=0.003695102 + y_offset, label = 'A'))
 
+ggsave(
+  file = 'varya.png', 
+  plot = graph_varya, 
+  width= 6, 
+  height = 4, 
+  path = 'intersection_plots'
+)
 
 # Varying Bankroll
 
-odds = .9
-probability = .6
-bankroll = seq(1, 1000, length.out = 1001)
-amount = c(1, 2, 5, 10, 25, 50, 100, "kelly")
-univ = expand.grid(odds = odds, probability = probability, 
-                   bankroll = bankroll, amount = amount, 
-                   stringsAsFactors = FALSE) %>%
+odds <- .9
+probability <- .6
+bankroll <- seq(1, 1000, length.out = 1001)
+amount <- c(1, 2, 5, 10, 25, 50, 100, "kelly")
+univ <- 
+  expand.grid(
+  odds = odds, 
+  robability = probability, 
+  bankroll = bankroll, 
+  amount = amount, 
+  stringsAsFactors = FALSE
+  ) %>%
   mutate(
     amount_color = as.factor(amount),
-    amount = ifelse(amount == "kelly", kelly(odds, probability) * bankroll, as.integer(amount)),
-    utility = log_utility(bankroll = bankroll, odds = odds, 
-                          probability = probability, amount = amount)) %>%
-  mutate(is_kelly = ifelse(amount_color == "kelly", "Kelly", "non-Kelly"))
+    amount = iflelse(amount == "kelly", kelly(odds, probability) * bankroll, as.integer(amount)),
+    utility = log_utility(
+      bankroll = bankroll, 
+      odds = odds, 
+      probability = probability, 
+      amount = amount
+      )
+    ) %>%
+  mutate(is_kelly = iflelse(amount_color == "kelly", "Kelly", "non-Kelly"))
 
-maxes = univ %>%
+maxes <- univ %>%
   filter(is_kelly == "non-Kelly") %>%
   group_by(odds, probability, bankroll) %>%
   slice(which.max(utility)) %>%
@@ -162,21 +186,25 @@ maxes = univ %>%
 x_offset = 5
 y_offset = -.0005
 
-univ %>%
+my_colors <- RColorBrewer::brewer.pal(8, "Dark2")
+names(my_colors) <- univ$amount_color %>%
+  levels()
+
+graph_varyb <- 
+  univ %>%
   left_join(maxes) %>%
   ggplot(aes(x = bankroll, y = utility)) +
   geom_line(aes(color = amount_color, linetype = is_kelly), size = 1) + 
   # geom_ribbon(aes(ymin = 0, ymax = best_utility, fill = best_bet), alpha = .2) + 
   geom_ribbon(aes(ymin = 0, ymax = .011, fill = best_bet), alpha = .1) + 
-  
   ylim(0, .011) + 
   xlim(0, 600) +
   scale_colour_manual(name  ="Bet Amount", breaks = c("1", "2", "5", "10", "25", "50", "100", "kelly"), values = my_colors) +
   scale_fill_manual(values = my_colors) +
   scale_linetype_manual(values = c("Kelly" = "dashed", "non-Kelly" = "solid")) + 
   guides(linetype=FALSE, fill = FALSE) +
-  xlab("Bankroll") + ylab("Logarithmic Utility") + 
-  ggtitle("Expected Logarithmic Utility with Varying Bankroll", subtitle = "(Probability = .6, Odds = .9)") + 
+  xlab("Bankroll") + ylab("Expected Logarithmic Utility") + 
+  # ggtitle("Expected Logarithmic Utility with Varying Bankroll", subtitle = "(Probability = .6, Odds = .9)") + 
   theme_bw() + 
   geom_point(aes(x=241.618691, y=0.009733776)) +
   geom_text(aes(x=241.618691 + x_offset, y=0.009733776 + y_offset, label = 'B')) +
@@ -184,3 +212,11 @@ univ %>%
   geom_text(aes(x=483.237382 + x_offset, y=0.009733776 + y_offset, label = 'C')) +
   geom_point(aes(x=400, y=0.01055328)) +
   geom_text(aes(x=400 + x_offset, y=0.01055328 + y_offset, label = 'A'))
+
+ggsave(
+  file = 'varyb.png', 
+  plot = graph_varyb, 
+  width= 6, 
+  height = 4, 
+  path = 'intersection_plots'
+)
