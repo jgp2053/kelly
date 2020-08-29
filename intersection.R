108d2790063ac369374cce735bed677af160173f
Jeff Phillips <jgp2053@gmail.com>
2020-08-28 22:46:22 -0400

    clean up formatting

--- intersection.R
+++ intersection.R
@@ -4,9 +4,11 @@ find_intersection_points <- function(probability, odds, bets){
   
   # initialize log utility function
   log_utility <- function(bankroll, amount, probability, odds) {
-    ifelse(amount >= bankroll, NA, 
-           probability * log(1 + odds * amount / bankroll) + 
-             (1 - probability) * log(1 - amount / bankroll))
+    ifelse(
+      amount >= bankroll, 
+      NA, 
+      probability * log(1 + odds * amount / bankroll) + 
+      (1 - probability) * log(1 - amount / bankroll))
   }
   
   num_bets <- length(bets)
@@ -23,10 +25,15 @@ find_intersection_points <- function(probability, odds, bets){
   # maximum to bound the algorithm
   dual.df %>%
     rowwise() %>%
-    mutate(intersection = 
-             uniroot(function(x) log_utility(x, bet_2, probability, odds) - 
-                                 log_utility(x, bet_1, probability, odds), 
-                                 interval = c(newton_lower, newton_higher)) %>% 
-             magrittr::extract2('root')) %>%
+    mutate(
+      intersection =
+        uniroot(
+          function(x) 
+            log_utility(x, bet_2, probability, odds) - 
+            log_utility(x, bet_1, probability, odds), 
+            interval = c(newton_lower, newton_higher)
+          ) %>% 
+        magrittr::extract2('root')
+      ) %>%
     select(bet_1, bet_2, intersection)
 }
